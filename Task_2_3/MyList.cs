122fee52e9c2af0124d579de7605120fe970fac8
iodandidiot <iodandidiot@gmail.com>
2016-04-12 01:28:31 +0300

    2_3a 2_3b

---
+++ Task_2_3/MyList.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace Task_2_3
+{
+    class MyList:IList
+    {
+        private object[] myListOdjects;
+        private int _count;
+
+        public MyList(int Length)
+        {
+            myListOdjects = new object[Length];
+            _count = 0;
+        }
+
+        public int Add(object newObject)
+        {
+            if (_count < myListOdjects.Length)
+            {
+                myListOdjects[_count] = newObject;
+                _count++;
+                return _count;
+            }
+            else
+            {
+                return -1;
+            }
+        }
+
+        public int Count
+        {
+            get
+            {
+                return _count;
+            }
+        }
+
+        public void CopyTo(Array array, int index)
+        {
+            foreach (object i in myListOdjects)
+            {
+                if (i == null) break;
+                array.SetValue(i, index);
+                index++;
+            }
+        }
+        public void Clear()
+        {
+            _count = 0;
+            myListOdjects = new object[myListOdjects.Length];
+        }
+        public bool Contains(object obj)
+        {
+            bool contains=false;
+
+            foreach (object i in myListOdjects)
+            {
+
+                if (i == obj) 
+                {
+                    contains = true;
+                    break;
+                }
+                    
+            }
+            return contains;
+        }        
+
+        public int IndexOf(object obj)
+        {
+            int indexOf=-1;
+            for (int i = 0; i < myListOdjects.Length; i++)
+            {
+                if (myListOdjects[i] == obj)
+                {
+                    indexOf = i;
+                    break;
+                }                    
+            }
+            return indexOf;
+        }
+
+        public IEnumerator GetEnumerator()
+        {
+            foreach (object obj in myListOdjects)
+            {
+                yield return obj;
+            }
+
+        }
+        public void Insert(int index, object obj)
+        {
+            if ((index != 0) || (index < _count))
+            {
+                if (_count + 1 <= myListOdjects.Length)
+                {
+                    for (int i = _count - 1; i > index; i--)
+                    {
+                        myListOdjects[i + 1] = myListOdjects[i];
+                    }
+                    _count++;
+                    myListOdjects[index] = obj;
+                }
+                else
+                {
+                    Console.WriteLine("Array is crowded");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Index over the limit");
+            }
+            
+        }
+        public void Remove(object obj)
+        {
+            RemoveAt(IndexOf(obj));
+        }
+
+        public void RemoveAt(int index)
+        {
+            if ((index < _count) && (index >=0))
+            {
+                for (int i = index; i < _count - 1; i++)
+                {
+                    myListOdjects[i] = myListOdjects[i + 1];
+                }
+                _count--;
+            }
+        }
+
+        public void Print()
+        {
+            foreach (object i in myListOdjects)
+            {
+                if (i == null) break;
+                Console.WriteLine(i);
+
+            }
+        }
+
+        public object this[int index]
+        {
+            get
+            {
+                return myListOdjects[index];
+            }
+            set
+            {
+                myListOdjects[index] = value;
+            }
+        }
+
+        public bool IsFixedSize
+        {
+            get
+            {
+                return true;
+            }
+        }
+        public object SyncRoot
+        {
+            get
+            {
+                return this;
+            }
+        }
+        public bool IsSynchronized
+        {
+            get
+            {
+                return false;
+            }
+        }
+        public bool IsReadOnly
+        {
+            get
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ Task_2_3/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Task_2_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please write the string");
+            MessageBoxOutput.Message(Console.ReadLine());
+            Console.WriteLine("Create and testing class MyList");
+            MyList _myList = new MyList(10);
+
+            Console.WriteLine("Add 3 objects in _myList");
+            _myList.Add ("string1");
+            _myList.Add(6);
+            _myList.Add(9.9);
+            _myList.Print();
+
+            WriteTestName("Count");
+            Console.WriteLine("_myList.Count - {0}", _myList.Count);
+
+            WriteTestName("CopyTo");
+            object[] _array=new object[3];
+            _myList.CopyTo(_array, 0);
+            foreach (object i in _array)
+            {
+                Console.WriteLine(i);
+            }
+
+            WriteTestName("Clear");
+            _myList.Clear();
+            _myList.Print();
+
+            Console.WriteLine("Add 3 new objects in _myList");
+            _myList.Add("string2");
+            _myList.Add(6.6f);
+            _myList.Add(9);
+            _myList.Print();
+
+            WriteTestName("Indexer");
+            Console.WriteLine("_myList[0] - {0}", _myList[0]);
+            _myList[3] = "string3";
+            _myList.Print();
+
+            WriteTestName("Contains");
+
+            Console.WriteLine("Is 'string6' contains in myList - {0}", _myList.Contains("string6"));
+            Console.WriteLine("Is 'string3' contains in myList - {0}", _myList.Contains("string3"));
+
+            WriteTestName("IndexOf");
+            Console.WriteLine("IndexOf string2 - {0}", _myList.IndexOf("string2"));
+
+
+            WriteTestName("Insert");
+            _myList.Insert(2, "string4");
+            _myList.Print();
+
+
+            Console.ReadLine();
+        }
+
+        static void WriteTestName(string test)
+        {
+            Console.WriteLine("Test function  " + test);
+            Console.WriteLine();
+        }
+    }
+
+    class MessageBoxOutput
+    {
+        static public void Message(string str)
+        {
+            MessageBox.Show(str);
+        }
+    }
+}
